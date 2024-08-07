871729dc87d094ed4a5ec9d490a99bb1ff0ae068
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-07 23:41:07 +0530

    s

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,13 @@
-import React, {useState} from "react";
+import React from 'react';
 import './../styles/App.css';
-import Child from "./child.js";
+import Child from './child';
 
 const App = () => {
-  const [todos, setTodos] =useState([{id:1, t: "learn React",complated:false }, {id:2, t: "Build a React app" , complated:false }, {id:3, t: "Deploy the React app", complated:false }]);
-  function handleComplete(id){
-    
-        setTodos(
-          todos.map((elem)=>{
-      if(elem.id==id){
-        return {...elem, completed:true};
-      }else{
-        return{...elem}
-      }
-    })
-  )
-  }
   return (
-    <>
-   
-        <Child datas={todos} handleComplete={handleComplete} />
-     
-    </>
+    <div>
+     <Child />
+    </div>
   )
-};
+}
 
 export default App;
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,24 +1,21 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-const Child = ({ datas, handleComplete}) => {
-  console.log('Child component props:', datas,handleComplete);
+const Child = () => {
+  const [mark, setmark]=useState(false);
   
-
- 
   return (
-    
-      datas.map((elem)=>(
-          <div style={{display:"flex",gap:"10px"}}>
-          <li>{elem.t}</li>
-      
-       { !elem.completed && 
-      <button onClick={()=>handleComplete(elem.id)}>complete</button>}
-      </div>
-    )
-  
-      
-        ))
-      }
-
+    <div>
+       <ul>
+        <h1>Child Component</h1>
+        {["Learn React", "Build a React app", "Deploy a React app"].map((list,idx)=>(
+          <div key={idx}>
+          <p>{list}</p>
+          {!mark && <button onClick={()=>setmark(true)}>Completed</button>}
+          </div>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default Child;
